b71627b80dda6f00f427fc61702c5dd1edd06d98
Attical <81836258+Attical@users.noreply.github.com>
2021-11-25 09:58:35 +0000

    changes

---
+++ src/test.py
@@ -0,0 +1,24 @@
+import unittest
+from PlayingCard import *
+from BlackJack import *
+
+class Test(unittest.TestCase):
+
+    test = BlackJack()
+
+    def test_passing(self):
+        pass
+
+    def test_failing(self):
+        if string == "hello":
+            return
+        
+    
+    def test_thing(self):
+        result = self.test.find_winner()
+
+def main():
+    unittest.main()
+
+if __name__ == "__main__":
+    unittest.main()
